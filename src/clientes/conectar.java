6613f36a473ddfa96b76d96800bdc2ec81e32d95
Mauricio Acevedo Rueda <maccevedor@gmail.com>
2013-11-20 19:28:44 -0500

    Se agrego el formulario de Cliente

---
+++ src/clientes/conectar.java
@@ -0,0 +1,32 @@
+package clientes;
+
+import java.sql.*;
+import javax.swing.*;
+/**
+ *
+ * @author Sony
+ */
+
+public class conectar {
+Connection conect = null;
+   public Connection conexion()
+    {
+      try {
+             
+           //Cargamos el Driver MySQL
+           Class.forName("org.gjt.mm.mysql.Driver");
+           conect = DriverManager.getConnection("jdbc:mysql://127.0.0.1/cliente","root","");
+           //JOptionPane.showMessageDialog(null, "Conectado");
+           //Cargamos el Driver Access
+           //Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+           //Conectar en red base 
+           //String strConect = "jdbc:odbc:Driver=Microsoft Access Driver (*.mdb);DBQ=//servidor/bd_cw/cw.mdb";
+           //Conectar Localmente
+           //String strConect = "jdbc:odbc:Driver=Microsoft Access Driver (*.mdb);DBQ=D:/cwnetbeans/cw.mdb";
+          //conect = DriverManager.getConnection(strConect,"",""); 
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Error "+e);
+        }
+        return conect;
+     
+}}
