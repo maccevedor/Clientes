6833374c05f9db3628ccd0822b8924447af561d1
unknown <macevedor@Chi11.chi.procol.com.co>
2013-11-26 17:21:34 -0500

    Configurada la base datos , realizacion de backup

--- src/formularios/ClassAcceso.java
+++ src/formularios/ClassAcceso.java
@@ -8,6 +8,8 @@ package formularios;
 import java.sql.*;
 import javax.swing.JOptionPane;
 import formularios.Cliente;
+import Control.conectar;
+
 public class ClassAcceso {
   public  void acceso(String usuario ,String clave){
 Connection conexio=null;
@@ -15,8 +17,10 @@ Statement consulta=null;
 ResultSet tabla=null;
 try{
     Class.forName("com.mysql.jdbc.Driver");
-    conexio=DriverManager.getConnection("jdbc:mysql://localhost/BD_SistemaBahia","root","");
+    conexio=DriverManager.getConnection("jdbc:mysql://localhost/cliente","root","");
     consulta=conexio.createStatement();
+    
+    
     tabla=consulta.executeQuery("select nom_usuario , cla_usuario from usuarios where nom_usuario='"+usuario+"'and cla_usuario='"+clave+"'");
     if(tabla.next()){
          JOptionPane.showMessageDialog(null, "logueo correcto");
